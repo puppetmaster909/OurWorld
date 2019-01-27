547944ec6548150486e5e6712ec123c072a42d62
Puppetmaster909 <pringthomas4@gmail.com>
2019-01-27 15:12:09 -0500

    IT WORKS v1

--- Assets/AirConsole/OurWorld/Scripts/GameLogic.cs
+++ Assets/AirConsole/OurWorld/Scripts/GameLogic.cs
@@ -7,26 +7,100 @@ using Newtonsoft.Json.Linq;
 public class GameLogic : MonoBehaviour
 {
     public Spawnables spawnList;
+    public AirConsole myConsole;
     
+    
+
+    List<int> playerIds = new List<int>();
+    Dictionary<int, bool> playersVoted = new Dictionary<int, bool>();
+
     void Awake()
     {
         AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onReady += OnReady;
+        AirConsole.instance.onConnect += OnConnect;
+    }
+
+    void OnReady(string code)
+    {
+        List<int> connectedDevices = AirConsole.instance.GetControllerDeviceIds();
+        foreach(int deviceID in connectedDevices)
+        {
+            AddNewPlayer(deviceID);
+        }
+    }
+
+    void OnConnect(int device)
+    {
+        AddNewPlayer(device);
+    }
+
+    private void AddNewPlayer(int deviceID)
+    {
+        if (playerIds.Contains(deviceID))
+        {
+            return;
+        }
+
+        playerIds.Add(deviceID);
+        playersVoted.Add(deviceID, false);
+
     }
 
     void OnMessage(int fromDeviceID, JToken data)
     {
         // DEBUG FOR CHECKING WHICH PLAYER SENT WHAT DATA
         //Debug.Log("message from " + fromDeviceID + ", data: " + data);
-        /*if (data["action"] != null && data["action"].ToString().Equals("interact"))
-        {
-            Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        }*/
 
-        if (data["action"] != null)
+        if (playerIds.Contains(fromDeviceID) && data["action"] != null)
         {
-            spawnList.ButtonInput(data["action"].ToString());
+            if (playersVoted[fromDeviceID] == false)
+            {
+                playersVoted[fromDeviceID] = spawnList.ButtonInput(fromDeviceID, data["action"].ToString());
+            }
+        }
+        
+        if (CheckIfAllVoted(playersVoted) == true)
+        {
+            spawnList.SpawnObject();
+
+            ClearVotes(playersVoted);
+            Debug.Log("Players may now vote again!");
         }
 
+    }
+
+    void ClearVotes(Dictionary<int, bool> playersVoted)
+    {
+        List<int> temp = new List<int>();
+        foreach(KeyValuePair<int, bool> player in playersVoted)
+        {
+            temp.Add(player.Key);
+        }
+        foreach(int t in temp)
+        {
+            playersVoted[t] = false;
+        }
+    }
+
+    bool CheckIfAllVoted(Dictionary<int, bool> playersVoted)
+    {
+        int votedCount = 0;
+        foreach(KeyValuePair<int, bool> entry in playersVoted)
+        {
+            if (entry.Value == true)
+            {
+                votedCount++;
+            }
+        }
+        //Debug.Log("Voted Count: " + votedCount);
+        if (votedCount == playersVoted.Count)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 
     void OnDestroy()
@@ -34,6 +108,8 @@ public class GameLogic : MonoBehaviour
         if (AirConsole.instance != null)
         {
             AirConsole.instance.onMessage -= OnMessage;
+            AirConsole.instance.onReady -= OnReady;
+            AirConsole.instance.onConnect -= OnConnect;
         }
     }
 }
--- Assets/AirConsole/OurWorld/Scripts/Spawnables.cs
+++ Assets/AirConsole/OurWorld/Scripts/Spawnables.cs
@@ -1,47 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Spawnables : MonoBehaviour
 {
     public GameObject square;
     public GameObject cylinder;
     public GameObject origin;
-
-    //public Transform[] spawnPoints;
     public List<Transform> spawnPoints = new List<Transform>();
+    public Text votingStatus;
 
+    private Dictionary<int, string> votes = new Dictionary<int, string>();
     private bool validSpawn = false;
-    private Transform spawnPT;
+    //private Transform spawnPT;
     private List<string> usedSpawnPTS = new List<string>();
+    private List<string> firstPair = new List<string>();
 
     List<GameObject> spawnableObjects = new List<GameObject>();
 
-    public void ButtonInput(string input)
+    public bool ButtonInput(int from, string input)
     {
         switch (input)
         {
             case "cylinder":
-                spawnObject(input);
-                break;
+                SubmitVote(from, input);
+                return true;
             case "square":
-                spawnObject(input);
-                break;
+                SubmitVote(from, input);
+                return true;
             case "button-up":
-                break;
+                return false;
             default:
                 Debug.Log("ERROR AT BUTTON INPUT");
-                break;
+                return false;
         }
     }
 
-    public void spawnObject(string objectName)
+    public void SubmitVote(int from, string objectName)
     {
+        if (votes.ContainsKey(from))
+        {
+            Debug.Log(from + " Already Voted for: " + objectName);
+        }
+        else
+        {
+            votes.Add(from, objectName);
+        }
+
+        //Debug.Log("Current Spawn PT: " + spawnPointIndex);
+        //Instantiate(temp, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation, origin.transform);
+    }
+
+    public void SpawnObject()
+    {
+        //count up votes
+        string objectName = "";
+        string option1Name = "";
+        string option2Name = "";
+        int option1Count = 0;
+        int option2Count = 0;
+
+        foreach (KeyValuePair<int, string> entry in votes)
+        {
+            if (option1Name == "")
+            {
+                option1Name = entry.Value;
+                option1Count++;
+                continue;
+            } else if (option2Name == "" && entry.Value != option1Name)
+            {
+                option2Name = entry.Value;
+                option2Count++;
+                continue;
+            } else if (option2Name == "" && entry.Value == option1Name)
+            {
+                if (firstPair.Contains(option1Name))
+                {
+                    int index = firstPair.IndexOf(option1Name);
+                    option2Name = firstPair[firstPair.Count - index - 1];
+                }
+            }
+
+            if (entry.Value == option1Name)
+            {
+                option1Count++;
+            } else if (entry.Value == option2Name)
+            {
+                option2Count++;
+            }
+        }
+        //Ballots counted
+        Debug.Log("1: " + option2Count + " 2: " + option2Count);
+        if (option1Count > option2Count)
+        {
+            objectName = option1Name;
+        } else if (option1Count < option2Count)
+        {
+            objectName = option2Name;
+        } else
+        {
+            votingStatus.text = "Tie, please vote again!";
+            votes.Clear();
+            return;
+        }
+
+        //Spawning section
         GameObject temp = new GameObject();
 
         for (int i = 0; i < spawnableObjects.Count; i++)
         {
-            if(spawnableObjects[i].name == objectName)
+            if (spawnableObjects[i].name == objectName)
             {
                 temp = spawnableObjects[i];
             }
@@ -50,28 +119,28 @@ public class Spawnables : MonoBehaviour
         int spawnPointIndex = Random.Range(0, spawnPoints.Count);
 
         while (validSpawn == false)
-         {
+        {
             if (usedSpawnPTS.Contains(spawnPoints[spawnPointIndex].name))
-             {
+            {
                 spawnPointIndex = Random.Range(0, spawnPoints.Count);
                 validSpawn = false;
-             } else
-             {
+            }
+            else
+            {
                 Instantiate(temp, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
                 usedSpawnPTS.Add(spawnPoints[spawnPointIndex].name);
-                Debug.Log("SpawnPT Name: " + spawnPoints[spawnPointIndex].name);
+                //Debug.Log("SpawnPT Name: " + spawnPoints[spawnPointIndex].name);
                 validSpawn = true;
-             }
-            if(usedSpawnPTS.Count == spawnPoints.Count)
+            }
+            if (usedSpawnPTS.Count == spawnPoints.Count)
             {
                 Debug.Log("Planet's full!");
                 break;
             }
-         }
+        }
+        votingStatus.text = "You've chosen: " + objectName;
         validSpawn = false;
-
-        //Debug.Log("Current Spawn PT: " + spawnPointIndex);
-        //Instantiate(temp, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation, origin.transform);
+        votes.Clear();
     }
 
     // Start is called before the first frame update
@@ -79,6 +148,11 @@ public class Spawnables : MonoBehaviour
     {
         spawnableObjects.Add(square);
         spawnableObjects.Add(cylinder);
+        votingStatus.text = "Please cast your votes!";
+
+        //Jank below
+        firstPair.Add("cylinder");
+        firstPair.Add("square");
     }
 
     // Update is called once per frame
@@ -86,4 +160,5 @@ public class Spawnables : MonoBehaviour
     {
         
     }
+
 }
